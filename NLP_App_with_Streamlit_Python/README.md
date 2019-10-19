e66d7aed967f6a6158d2517a884c04468ae2df68
JCharis Jesse <jcharistech@gmail.com>
2019-10-19 20:40:00 +0300

    NLP App with Streamlit
    NLP app- SpaCy NER,Sentiment Analysis,Summarization App

---
+++ NLP_App_with_Streamlit_Python/README.md
@@ -0,0 +1,31 @@
+### NLP Based App with Streamlit
++ (NLPiffy) -Natural Language Processing on the Go
+
+##### To Run the App
+```bash
+streamlit run app.py
+```
+
+#### NLP App
+![](images/image01.png)
+
+
+#### Tokenization
+![](images/image01.png)
+
+#### Tokenization
+![](images/image02.png)
+
+#### Entity Extraction with Spacy
+![](images/image03.png)
+
+#### Text Summarization
+![](images/image04.png)
+
+#### NLP App
+![](images/image05.png)
+
+
+#### By
++ Jesse E.Agbe(JCharis)
++ Jesus Saves @JCharistech
---
+++ NLP_App_with_Streamlit_Python/app.py
@@ -0,0 +1,114 @@
+import streamlit as st 
+import os
+
+
+# NLP Pkgs
+from textblob import TextBlob 
+import spacy
+from gensim.summarization import summarize
+
+# Sumy Pkg
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+
+
+# Sumy 
+def sumy_summarizer(docx):
+	parser = PlaintextParser.from_string(docx,Tokenizer("english"))
+	lex_summarizer = LexRankSummarizer()
+	summary = lex_summarizer(parser.document,3)
+	summary_list = [str(sentence) for sentence in summary]
+	result = ' '.join(summary_list)
+	return result
+
+# Analyse Tokens and Lemma
+@st.cache
+def text_analyzer(my_text):
+	nlp = spacy.load('en')
+	docx = nlp(my_text)
+	# tokens = [ token.text for token in docx]
+	allData = [('"Token":{},\n"Lemma":{}'.format(token.text,token.lemma_))for token in docx ]
+	return allData
+
+# Function For Extracting Entities
+@st.cache
+def entity_analyzer(my_text):
+	nlp = spacy.load('en')
+	docx = nlp(my_text)
+	tokens = [ token.text for token in docx]
+	entities = [(entity.text,entity.label_)for entity in docx.ents]
+	allData = ['"Token":{},\n"Entities":{}'.format(tokens,entities)]
+	return allData
+
+
+def main():
+	""" NLP Based App with Streamlit """
+
+	# Title
+	st.title("NLPiffy with Streamlit")
+	st.subheader("Natural Language Processing On the Go..")
+
+	# Tokenization
+	if st.checkbox("Show Tokens and Lemma"):
+		st.subheader("Tokenize Your Text")
+
+		message = st.text_area("Enter Text","Type Here ..")
+		if st.button("Analyze"):
+			nlp_result = text_analyzer(message)
+			st.json(nlp_result)
+
+	# Entity Extraction
+	if st.checkbox("Show Named Entities"):
+		st.subheader("Analyze Your Text")
+
+		message = st.text_area("Enter Text","Type Here ..")
+		if st.button("Extract"):
+			entity_result = entity_analyzer(message)
+			st.json(entity_result)
+
+	# Sentiment Analysis
+	if st.checkbox("Show Sentiment Analysis"):
+		st.subheader("Analyse Your Text")
+
+		message = st.text_area("Enter Text","Type Here ..")
+		if st.button("Analyze"):
+			blob = TextBlob(message)
+			result_sentiment = blob.sentiment
+			st.success(result_sentiment)
+
+	# Summarization
+	if st.checkbox("Show Text Summarization"):
+		st.subheader("Summarize Your Text")
+
+		message = st.text_area("Enter Text","Type Here ..")
+		summary_options = st.selectbox("Choose Summarizer",['sumy','gensim'])
+		if st.button("Summarize"):
+			if summary_options == 'sumy':
+				st.text("Using Sumy Summarizer ..")
+				summary_result = sumy_summarizer(message)
+			elif summary_options == 'gensim':
+				st.text("Using Gensim Summarizer ..")
+				summary_result = summarize(rawtext)
+			else:
+				st.warning("Using Default Summarizer")
+				st.text("Using Gensim Summarizer ..")
+				summary_result = summarize(rawtext)
+
+		
+			st.success(summary_result)
+
+
+
+	st.sidebar.subheader("About App")
+	st.sidebar.text("NLPiffy App with Streamlit")
+	st.sidebar.info("Cudos to the Streamlit Team")
+	
+
+	st.sidebar.subheader("By")
+	st.sidebar.text("Jesse E.Agbe(JCharis)")
+	st.sidebar.text("Jesus saves@JCharisTech")
+	
+
+if __name__ == '__main__':
+	main()
