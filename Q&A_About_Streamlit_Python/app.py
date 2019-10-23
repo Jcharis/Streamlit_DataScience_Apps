1bc9def699e2ebb29c14ac6d3f36c45ab3f8f4f7
JCharis Jesse <jcharistech@gmail.com>
2019-10-23 06:38:57 +0300

    Added How to Upload on Streamlit Apps

--- Q&A_About_Streamlit_Python/app.py
+++ Q&A_About_Streamlit_Python/app.py
@@ -39,3 +39,15 @@ st.subheader("How to Receive User Input")
 name = st.text_input("Enter Your Name","Type Here")
 result_name = name.title()
 st.write(result_name)
+
+## How to do Upload of Files
+# Solution By Adrien Treuile @streamlit
+#NB New Updates may include this feature request
+def file_selector(folder_path='.'):
+	    filenames = os.listdir(folder_path)
+	    selected_filename = st.selectbox('Select a file', filenames)
+	    return os.path.join(folder_path, selected_filename)
+
+	filename = file_selector()
+	st.write('You selected `%s`' % filename)
+
