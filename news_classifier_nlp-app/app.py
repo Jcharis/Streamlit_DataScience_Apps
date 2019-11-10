11899b4dd316cb81bcf0f9df2a2e0028ee467e06
JCharis <jcharistech@gmail.com>
2019-11-10 16:01:29 +0200

    News Classification ML and NLP Streamlit App

---
+++ news_classifier_nlp-app/app.py
@@ -0,0 +1,113 @@
+import streamlit as st 
+import joblib,os
+import spacy
+import pandas as pd
+nlp = spacy.load('en')
+
+# load Vectorizer For Gender Prediction
+news_vectorizer = open("models/final_news_cv_vectorizer.pkl","rb")
+news_cv = joblib.load(news_vectorizer)
+
+# # load Model For Gender Prediction
+# news_nv_model = open("models/naivebayesgendermodel.pkl","rb")
+# news_clf = joblib.load(news_nv_model)
+
+def load_prediction_models(model_file):
+	loaded_model = joblib.load(open(os.path.join(model_file),"rb"))
+	return loaded_model
+
+
+
+# Get the Keys
+def get_key(val,my_dict):
+	for key,value in my_dict.items():
+		if val == value:
+			return key
+
+
+
+def main():
+	"""News Classifier"""
+	st.title("News Classifier")
+	st.subheader("ML App with Streamlit")
+
+	activity = ['Prediction','NLP']
+	choice = st.sidebar.selectbox("Select Activity",activity)
+
+
+	if choice == 'Prediction':
+		st.info("Prediction with ML")
+
+		news_text = st.text_area("Enter News Here","Type Here")
+		all_ml_models = ["LR","RFOREST","NB","DECISION_TREE"]
+		model_choice = st.selectbox("Select Model",all_ml_models)
+
+		prediction_labels = {'business': 0,'tech': 1,'sport': 2,'health': 3,'politics': 4,'entertainment': 5}
+		if st.button("Classify"):
+			st.text("Original Text::\n{}".format(news_text))
+			vect_text = news_cv.transform([news_text]).toarray()
+			if model_choice == 'LR':
+				predictor = load_prediction_models("models/newsclassifier_Logit_model.pkl")
+				prediction = predictor.predict(vect_text)
+				st.write(prediction)
+			elif model_choice == 'RFOREST':
+				predictor = load_prediction_models("models/newsclassifier_RFOREST_model.pkl")
+				prediction = predictor.predict(vect_text)
+				st.write(prediction)
+			elif model_choice == 'NB':
+				predictor = load_prediction_models("models/newsclassifier_NB_model.pkl")
+				prediction = predictor.predict(vect_text)
+				st.write(prediction)
+			elif model_choice == 'DECISION_TREE':
+				predictor = load_prediction_models("models/newsclassifier_CART_model.pkl")
+				prediction = predictor.predict(vect_text)
+				st.write(prediction)
+
+			final_result = get_key(prediction,prediction_labels)
+			st.success("News Categorized as:: {}".format(final_result))
+
+	if choice == 'NLP':
+		st.info("Natural Language Processing of Text")
+		raw_text = st.text_area("Enter News Here","Type Here")
+		nlp_task = ["Tokenization","Lemmatization","NER","POS Tags"]
+		task_choice = st.selectbox("Choose NLP Task",nlp_task)
+		if st.button("Analyze"):
+			st.info("Original Text::\n{}".format(raw_text))
+
+			docx = nlp(raw_text)
+			if task_choice == 'Tokenization':
+				result = [token.text for token in docx ]
+			elif task_choice == 'Lemmatization':
+				result = ["'Token':{},'Lemma':{}".format(token.text,token.lemma_) for token in docx]
+			elif task_choice == 'NER':
+				result = [(entity.text,entity.label_)for entity in docx.ents]
+			elif task_choice == 'POS Tags':
+				result = ["'Token':{},'POS':{},'Dependency':{}".format(word.text,word.tag_,word.dep_) for word in docx]
+
+			st.json(result)
+
+		if st.button("Tabulize"):
+			docx = nlp(raw_text)
+			c_tokens = [token.text for token in docx ]
+			c_lemma = [token.lemma_ for token in docx ]
+			c_pos = [token.pos_ for token in docx ]
+
+			new_df = pd.DataFrame(zip(c_tokens,c_lemma,c_pos),columns=['Tokens','Lemma','POS'])
+			st.dataframe(new_df)
+
+
+
+
+
+
+
+	st.sidebar.subheader("About")
+
+
+
+
+if __name__ == '__main__':
+	main()
+
+# By Jesse E.Agbe(JCharis)
+# Jesus Saves@JCharisTech
